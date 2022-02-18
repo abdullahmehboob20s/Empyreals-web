da1b6bf8fd5cfe0625bd8db64141810246c693e1
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-02-18 11:06:19 +0500

    Done

--- src/layouts/HomePage/Collection/Collection.js
+++ src/layouts/HomePage/Collection/Collection.js
@@ -26,68 +26,64 @@ function Collection() {
         </div>
       </div>
 
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={60}
-        effect={"coverflow"}
-        centeredSlides={true}
-        loop={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 200,
-          modifier: 1,
-        }}
-        onSwiper={(swiper) => {
-          setTimeout(() => {
-            swiper.params.navigation.prevEl = navigationPrevRef.current;
-            swiper.params.navigation.nextEl = navigationNextRef.current;
-            swiper.navigation.destroy();
-            swiper.navigation.init();
-            swiper.navigation.update();
-          });
-        }}
-        navigation={{
-          prevEl: navigationPrevRef.current,
-          nextEl: navigationNextRef.current,
-        }}
-        className="collection-slider"
-        style={{ padding: isBellow500px ? "0 10%" : "" }}
-        breakpoints={{
-          800: {
-            spaceBetween: 60,
-            slidesPerView: 3,
-          },
-          600: {
-            spaceBetween: 30,
-            slidesPerView: 3,
-          },
-          500: {
-            spaceBetween: 40,
-            slidesPerView: 2,
-          },
-          200: {
-            spaceBetween: 50,
-            slidesPerView: 1,
-          },
-        }}
-      >
-        <SwiperSlide>
-          <Fade delay={500}>
+      <Fade direction="up" delay={300}>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={60}
+          effect={"coverflow"}
+          centeredSlides={true}
+          loop={true}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 200,
+            modifier: 1,
+          }}
+          onSwiper={(swiper) => {
+            setTimeout(() => {
+              swiper.params.navigation.prevEl = navigationPrevRef.current;
+              swiper.params.navigation.nextEl = navigationNextRef.current;
+              swiper.navigation.destroy();
+              swiper.navigation.init();
+              swiper.navigation.update();
+            });
+          }}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+          className="collection-slider"
+          style={{ padding: isBellow500px ? "0 10%" : "" }}
+          breakpoints={{
+            800: {
+              spaceBetween: 60,
+              slidesPerView: 3,
+            },
+            600: {
+              spaceBetween: 30,
+              slidesPerView: 3,
+            },
+            500: {
+              spaceBetween: 40,
+              slidesPerView: 2,
+            },
+            200: {
+              spaceBetween: 50,
+              slidesPerView: 1,
+            },
+          }}
+        >
+          <SwiperSlide>
             <img src={collection1} className="w-full" alt="" />
-          </Fade>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Fade delay={500}>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={collection2} className="w-full" alt="" />
-          </Fade>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Fade delay={500}>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={collection3} className="w-full" alt="" />
-          </Fade>
-        </SwiperSlide>
-      </Swiper>
+          </SwiperSlide>
+        </Swiper>
+      </Fade>
     </div>
   );
 }
